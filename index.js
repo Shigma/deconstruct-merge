8beae4a12926129fe13bb5c725fd63487cb1c92e
Shigma <1700011071@pku.edu.cn>
2019-03-11 00:42:25 +0800

    unregistered options assignment

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const flatten = require('array-flatten')
 
 module.exports = class Mergeable {
+  static getValue (source) {
+    if (!source || typeof source !== 'object' || !source._mergeable) {
+      return source
+    } else {
+      return source.value()
+    }
+  }
+
   constructor (options) {
     this._options = options
+    this._mergeable = true
     if (options === 'assign') {
       this._value = {}
       this._merge = function (value) {
@@ -19,20 +28,26 @@ module.exports = class Mergeable {
         this._value = options(value, this._value)
       }
     } else if (Array.isArray(options)) {
+      const { length } = options
       this._value = options.map(option => new Mergeable(option))
       this._merge = function (value) {
         if (!Array.isArray(value)) return
         this._value.forEach((item, index) => item.merge(value[index]))
+        this._value.splice(length, Infinity, ...value.slice(length))
       }
     } else if (options && typeof options === 'object') {
       this._value = {}
-      for (const key in this._options) {
-        this._value[key] = new Mergeable(this._options[key])
+      for (const key in options) {
+        this._value[key] = new Mergeable(options[key])
       }
       this._merge = function (value) {
         if (!value || typeof value !== 'object') return
-        for (const key in options) {
-          this._value[key].merge(value[key])
+        for (const key in value) {
+          if (key in options) {
+            this._value[key].merge(value[key])
+          } else {
+            this._value[key] = value[key]
+          }
         }
       }
     } else {
@@ -51,11 +66,11 @@ module.exports = class Mergeable {
     if (this._options === 'flat') {
       return flatten(this._value)
     } else if (Array.isArray(this._options)) {
-      return this._value.map(item => item.value())
+      return this._value.map(Mergeable.getValue)
     } else if (this._options && typeof this._options === 'object') {
       const value = {}
-      for (const key in this._options) {
-        value[key] = this._value[key].value()
+      for (const key in this._value) {
+        value[key] = Mergeable.getValue(this._value[key])
       }
       return value
     } else {
